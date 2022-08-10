1dc5bd0902cc5bb347c7f53da0c4f82d39d52b94
Quynh Anh <89251525+Quanh1012@users.noreply.github.com>
2022-08-10 14:13:28 +0700

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -18,13 +18,18 @@ class CustomDataset(Dataset):
     def __len__(self):
         return self.rolling_windows.shape[0]
     def __getitem__(self, index):
-        return self.rolling_windows[index]
+        return self.rolling_windows[index], self.sample[index]
     def load_dataset(self):
         #data_dir = self.config['data_dir']
         self.data = np.load(r"C:\Users\QUYNH ANH\pre_processing1.npz")
         
         if self.mode != "test":
-            data = self.data['training'][:10000]
+            data = self.data['training'][:10000,: -1]
+            self.sample = self.data['training'][self.config['l_win']: 10000 + self.config['l_win'],:-1]
+            
         else:
-            data = self.data['test'][:200]
+            data = self.data['test'][:1000,: -1]
+            self.sample = self.data['test'][120:1120,:-1]
+        
         self.rolling_windows = np.lib.stride_tricks.sliding_window_view(data, self.config['l_win'], axis = 0, writeable = True).transpose(0,2,1)
+        

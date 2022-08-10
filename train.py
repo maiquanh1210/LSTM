8a9a0804971c60b1eedfab5fb393a02a2605de73
Quynh Anh <89251525+Quanh1012@users.noreply.github.com>
2022-08-10 14:14:55 +0700

    Update train.py

--- train.py
+++ train.py
@@ -21,9 +21,9 @@ else:
     device = torch.device("cpu")
     
 class Trainer():
-    def __init__(self, model, device, optimize, config, mode='train'):
+    def __init__(self, model, device, train_data, optimize, config, mode='train'):
         self.model = model
-        #self.train_data = train_data
+        self.train_data = train_data
         self.device = device
         self.config = config
         self.optim = optimize
@@ -31,18 +31,18 @@ class Trainer():
         self.min_loss = float('inf')
         self.train_loss_list = []
         self.best_optimizer = None
+        
     def train_epoch(self, criterion, optim, epoch_idx):
         
         self.model.train()
         train_loss = 0.0
-        x = self.model.init_hidden()
-        for i, data in enumerate(self.train_data):
+        
+        for i, (data, sample) in enumerate(self.train_data):
             print(i)
-            label = data[:,:,-1]
             self.model.zero_grad()
-            
-            out = self.model(data[:,:,:-1].to(device).float(), x)
-            loss = criterion(out, label.to(device).float())
+            x = self.model.init_hidden(len(data))
+            out = self.model(data.to(device).float(), x)
+            loss = criterion(out, sample.to(device).float())
             loss.backward()
             optim.step()
             train_loss += loss.item()
@@ -52,46 +52,18 @@ class Trainer():
         print("Epoch {} done, Train loss: {}".format(epoch_idx, train_loss))
         return self.model
             
-    def train(self, train_data):
-        self.train_data = train_data
+    def train(self):
         self.model.to(self.device)
         start = time.perf_counter()
         print("Start training")
         model_opt = self.optim.Adam(self.model.parameters(), lr = self.config['lr'])
         criterion = nn.MSELoss()
+        
         for epoch in range(0, self.config["epoch_n"]):
             print("Training epoch {}".format(epoch + 1))
-            self.train_epoch(criterion, model_opt, epoch + 1)
+            self.train_epoch(criterion, model_opt, epoch)
             
         df_loss = pd.DataFrame(self.train_loss_list)
-        df_loss.to_csv("train_loss1.csv", mode = 'a', index = False)
+        df_loss.to_csv("train_loss_50epoch.csv", mode = 'a', index = False)
         print("time training: {}".format(time.perf_counter() - start))
-        torch.save(self.model.state_dict(), 'lstm_trained.pt')
-       # return self.model
-    
-    def test(self, test_data):
-        model = self.model
-        model.eval()
-        model.to(self.device)
-        output = []
-        sample = []
-        #criterion = nn.CrossEntropyLoss().to(self.device)
-        with torch.no_grad():
-            for data in test_data:
-                data_ = data[:,:,:-1].to(self.device).float()
-                target = data[:,:,-1].to(self.device).float()
-                x = model.init_hidden()
-                pred = model(data_.to(device).float(), x)
-                output.append(pred)
-                sample.append(target)
-        return output, sample
-                
-                
-                
-        
-        
-                
-            
-    
-        
-        
+        torch.save(self.model.state_dict(), 'lstm_trained_50epoch.pt')
